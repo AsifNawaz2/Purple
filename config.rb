00af512dbdb9ab3068c95e31f849bfdea5b24467
Asif Nawaz <asif.nawaz@infinikorn.com>
2022-10-05 12:37:45 +0500

    Add webpack with configuration

--- config.rb
+++ config.rb
@@ -9,6 +9,12 @@ configure :development do
   activate :livereload
 end
 
+activate :external_pipeline,
+          name: :webpack,
+          command: build? ? "yarn run build" : "yarn run start",
+          source: "dist",
+          latency: 1
+
 # Layouts
 # https://middlemanapp.com/basics/layouts/
 
